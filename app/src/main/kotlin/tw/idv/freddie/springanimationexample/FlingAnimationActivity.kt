e77676936bc52ad5ce7670452ca3965f6da1721e
Freddie Wang <wangyung@gmail.com>
2017-05-23 00:19:36 +0900

    Add basic files

---
+++ app/src/main/kotlin/tw/idv/freddie/springanimationexample/FlingAnimationActivity.kt
@@ -0,0 +1,10 @@
+package tw.idv.freddie.springanimationexample
+
+import android.app.Activity
+import android.os.Bundle
+
+class FlingAnimationActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/kotlin/tw/idv/freddie/springanimationexample/SpringAnimationActivity.kt
@@ -0,0 +1,10 @@
+package tw.idv.freddie.springanimationexample
+
+import android.app.Activity
+import android.os.Bundle
+
+class SpringAnimationActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
